59b23029f0fede8d57ef1cf61a324db69ab01e77
Unavailable Shorts <161657247+unavailableshorts@users.noreply.github.com>
2026-02-20 01:54:34 +0530

    Restrict Access-Control-Allow-Origin to specific origin

--- api/admin.js
+++ api/admin.js
@@ -127,7 +127,7 @@ export default async function handler(req, res) {
     return res.status(403).json({ error: "Access denied" });
   }
 
-  res.setHeader("Access-Control-Allow-Origin", origin || "*");
+  res.setHeader("Access-Control-Allow-Origin", origin);
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
